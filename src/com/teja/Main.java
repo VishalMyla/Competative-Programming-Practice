392b43df35cf909c66284c1a4b48630c1c92be93
vishalmyla <vishalmyla@gmail.com>
2022-04-02 23:05:35 +0530

    I'm reocording my compitative journey here,Hope it will be Helpful for all of you !!

---
+++ src/com/teja/Main.java
@@ -0,0 +1,8 @@
+package com.teja;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hey I'm just recording my competative programming journey !!");
+    }
+}
